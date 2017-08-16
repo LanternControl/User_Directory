de1d63817a465869a46abc2bffc61149eb30761a
LanternControl <brett4president@hotmail.com>
2017-08-16 14:03:36 -0400

    Got the data linked. Working on app.engine

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const mustache = require('mustache');
+const data = require('./data.js');
 const app = express();
+
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
